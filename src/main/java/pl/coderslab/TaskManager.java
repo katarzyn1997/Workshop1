9cb2df40616c436bacae25832beb21dd526c7462
katarzyna1997 <katarzyna.popek97@gmail.com>
2024-12-21 16:20:22 +0100

    method1v2

--- src/main/java/pl/coderslab/TaskManager.java
+++ src/main/java/pl/coderslab/TaskManager.java
@@ -3,13 +3,18 @@ package pl.coderslab;
 import static pl.coderslab.ConsoleColors.*;
 
 public class TaskManager {
+    //
+    static final String[] OPTION = {"add", "remove", "list", "exit"};
 
     public static void main(String[] args) {
-
+        showAllUserOption(OPTION);
     }
 
     public static void showAllUserOption(String[] arr){
         System.out.println(BLUE + "Please select an option: " + RESET);
+        for(String iterator:arr){
+            System.out.println(iterator);
+        }
 
     }
 }
